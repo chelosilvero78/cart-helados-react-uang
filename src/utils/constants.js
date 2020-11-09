b0a1d7598d7e30fd342f4142d710d720e1de4846
Marcelo Silvero <marcesilvero@gmail.com>
2020-11-08 22:58:45 -0300

    mi primer commit

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+//export const urlApiProducts = "https://api.myjson.com/bins/12nb8n";
+export const urlApiProducts = "https://demo2562028.mockable.io/products";
+//export const urlApiProducts ="http://localhost:8084/ai/tareasusuario/listar/todos";
+
+export const BASE_PATH = "http://localhost:3000";
+export const STORAGE_PRODUCTS_CART = "productsCart";
